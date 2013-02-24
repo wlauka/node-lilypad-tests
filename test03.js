a8311e219de331d81fbabb53382cebad0e8753cf
Wesley Lauka <wlauka@gmail.com>
2013-02-24 01:42:35 -0500

    test 03 - sensor data with socket.io

--- test03.js
+++ test03.js
@@ -1,19 +1,45 @@
 /* --------------------------------------------
 
-test02.js
+test03.js
 
-Node and Lilypad TEST NO. 02
+Node and Lilypad TEST NO. 03
 2/ 23 / 12
  
-Getting the strech sensor to work with johnny-five!
+Printing Sensor Data to the Screen
  	
 ----------------------------------------------- */
 
-var five = require('johnny-five'),
-	connect = require('connect');
+var http = require('http'),
+	five = require('johnny-five'),
+	connect = require('connect'),
+	io = require('socket.io');
 	
+/* set up the HTTP Server
+----------------------------------------------- */
+var app = http.createServer(
+  connect()
+  .use(connect.favicon())
+  .use(connect.logger('dev'))
+  .use(connect.query())
+  .use(function(req, res,next){
+    console.log(req.query);
+    next();
+  })
+  .use(connect.static('public'))
+);
 
-/* set up the lilypad
+var sio = io.listen(app); 
+app.listen(4000);
+
+/* Monitor Socket Communication
+----------------------------------------------- */
+
+sio.on('connection', function(socket) {
+	socket.emit('sensor', {output: '<span class="green">Connected</span>. Listening for Sensor Data...'});
+
+});
+
+/* Set Up the Lilypad
 ----------------------------------------------- */
 var board = new five.Board();
 
@@ -31,19 +57,9 @@ board.on("ready",function(){
 
 	cord.on('read', function(err, val){
 		console.log(val);
+		var out = val;
+		if(val < 930) out = '<span class="icon-ok">'+val+'</span>';
+		sio.sockets.emit('sensor',{output:out});
 	});
 
-}); // END board -> Ready
-
-/* set up the HTTP Server
------------------------------------------------ */
-var app = connect()
-  .use(connect.favicon())
-  .use(connect.logger('dev'))
-  .use(connect.query())
-  .use(function(req, res,next){
-    console.log(req.query);
-    next();
-  })
-   .use(connect.static('public'))
- .listen(4000);
+}); // END board -> Ready 
